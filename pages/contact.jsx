ccdde2a0ba5799136dfac5942c8844ee06ecefc2
amo2019 <aminvpython@gmail.com>
2022-02-05 00:44:02 +0100

    add contact page

---
+++ pages/contact.jsx
@@ -0,0 +1,26 @@
+import style from "../styles/Contact.module.css";
+
+const Contact = () => {
+  return (
+    <>
+    <div className={style.container}>
+      <h1 className={style.title}>Contact Us</h1>
+      <form className={style.form}>
+        <input className={style.inputSmall} type="text" placeholder="Name" />
+        <input className={style.inputSmall} type="text" placeholder="Phone" />
+        <input className={style.inputLarge} type="text" placeholder="Email" />
+        <input className={style.inputLarge} type="text" placeholder="Subject" />
+        <textarea
+          className={style.textArea}
+          type="text"
+          rows={6}
+          placeholder="Message"
+        />
+        <button className={style.button}>SUBMIT</button>
+      </form>
+    </div>
+    </>
+  );
+};
+
+export default Contact;
