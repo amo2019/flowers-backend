6649b52b4efa9470ca63d70cbaa8cf598987d87a
amo2019 <aminvpython@gmail.com>
2022-02-05 00:35:11 +0100

    edit _app & index.js & Layout.module.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import Layout from "../components/Layout";
 import '../styles/globals.css'
+import store from "../redux/store";
+import { Provider } from "react-redux";
+//import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 function MyApp({ Component, pageProps }) {
+  //const queryClient = new QueryClient()
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-  )
+    <Provider store={store}>
+      <Layout>
+      <Component {...pageProps} /> 
+      </Layout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
