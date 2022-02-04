edd21c8ae692f77f4c3738bfb0675c4d29625403
amo2019 <aminvpython@gmail.com>
2022-02-05 00:33:05 +0100

    edit Footer & Navbar & their css moduls

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,19 @@
 import style from "../styles/Footer.module.css";
-import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Footer() {
+
   return (    
+  <div className={style.wrapper}>
   <div className={style.container}>
     <h3 className={style.title}>FLOWERS.</h3>
     <h3 className={style.linkTitle}>
       <Link href="/contact" className={style.link} passHref>
-        <>
           <span className={style.linkText}>CONTACT US</span>
-        </>
       </Link>
     </h3>
+</div>
 </div>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,26 @@
-
 import React from 'react';
 import style from "../styles/Navbar.module.css";
 import Image from "next/image";
+import Link from "next/link";
+import { filteredProducts, filteredField } from "../redux/productSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Navbar() {
+  const searchField = useSelector((state) => state.product.searchField);
+  const dispatch = useDispatch();
   return (
   <nav>
   <div className={style.navTop}>
       <div className={style.navItem}>
-        <div className={style.navImage}>
-          <Image src="/img/gift.png"  
-                      className={style.navImage}
-                      width="60px"
-                      height="40px"
-                      layout="responsive"
-                      objectFit="cover"
-                      alt=""/>
-         </div>
+        <Link href="/" className={style.navImage} passHref>
+        <a>
+          <Image src="/img/flowers.svg"  height="50px" width="50px" alt=''/>
+        </a>
+        </Link>
       </div>
       <div className={style.navItem}>
           <div className={style.search}>
-              <input type="text" placeholder="Search" className={style.searchInput}/>
+              <input type="text" placeholder="Search" value={searchField} onChange={(e) => dispatch(filteredField(e.target.value))} className={style.searchInput}/>
               <Image src="/img/search.png" width="20" height="20" alt="" className={style.searchIcon}/>
           </div>
       </div>
@@ -31,5 +31,3 @@ export default function Navbar() {
   </nav>
 )
 }
-
-
