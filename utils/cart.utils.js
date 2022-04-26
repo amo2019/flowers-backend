fc6170f3f2fe9f2617c5e492e1ade6a32938396a
Amin <48186122+amo2019@users.noreply.github.com>
2022-04-27 00:05:46 +0200

    add manifest.json and icons for PWA

--- utils/cart.utils.js
+++ utils/cart.utils.js
@@ -13,6 +13,14 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+export const addItemsToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.productId === cartItemToAdd.productId
+  );
+
+if (!existingCartItem) return [...cartItems, { ...cartItemToAdd}];
+  return [...cartItems];
+};
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find(
@@ -29,3 +37,22 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
       : cartItem
   );
 };
+
+const formatMoney = function(amount) {
+  const formatter = Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 2
+  })
+  return formatter.format(amount)
+}
+
+export const subTotalFormatted = (cartItems) => {
+    let finalTotal = 0
+    let subTotatlFormatted = ''
+    cartItems.map((item) => {
+      finalTotal += item.quantity * item.price
+      subTotatlFormatted = `${formatMoney(finalTotal)}`
+    })
+    return subTotatlFormatted
+}
