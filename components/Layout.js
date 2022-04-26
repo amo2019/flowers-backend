a487d25f910a7972d3a03156c1cd79eb2ca6ad32
Amin <48186122+amo2019@users.noreply.github.com>
2022-04-27 00:02:01 +0200

    edit to add conditional hide of Navbar component

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,15 @@
 import styles from "../styles/Layout.module.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { useRouter } from 'next/router'
 
 const Layout = ({ children }) => {
+  const router = useRouter()
+  const match = router.pathname.includes('/admin')
   return (
     <div className={styles.layout}>
       <>
-        <Navbar />
+       {!match && <Navbar />}
         {children}
         <Footer/>
       </>
