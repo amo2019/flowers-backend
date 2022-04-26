7fb0ac7a84014c33a56c097f347049bb7e47e459
Amin <48186122+amo2019@users.noreply.github.com>
2022-04-26 23:55:49 +0200

    add total calculations to Cartdropdown component

--- components/cartDropdown/CartDropdown.jsx
+++ components/cartDropdown/CartDropdown.jsx
@@ -3,14 +3,38 @@ import CartItemComponent from "../cartItem/CartItemComponent";
 import style from "../../styles/cartDropdown.module.css";
 import CustomButton from '../customButton/CustomButton';
 import {
-  useCartItems, useClearCart, useRemoveFromCart
+  useCartItems, useClearCart, useAddItemsToChechoutCart
 } from "../../zustand-store"
+import {subTotalFormatted} from '../../utils/cart.utils'
+import Router from 'next/router'
+
 
 export default function CartDropdown({ toggleState, setToggleState }) {
   const items = useCartItems()
+  const subTotal = subTotalFormatted(items)
+
   useEffect(()=>{
   },[ items])
   const clearCart = useClearCart();
+  const addItemsToChechoutCart = useAddItemsToChechoutCart();
+  const handleCart = async (method) => {
+    try {
+      const req = await fetch('/api/order', {
+        method,
+        body: JSON.stringify({
+          userId: '18ec7f0c-68fc-4589-8294-06e6623971f4',
+        })
+      });
+      setToggleState(!toggleState);
+      const res = await req.json();
+      addItemsToChechoutCart(items);
+      Router.push("/checkout");
+      clearCart();
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
   <div className={style.cartDropdownContainer} >
     {items?.length ? (
@@ -22,18 +46,20 @@ export default function CartDropdown({ toggleState, setToggleState }) {
     ) : (
       <span className={style.emptyMessageContainer}> {false? 'Your cart is empty': 'Your cart is empty (please login)'}</span>
     )}
+    {
+      subTotal? <span className={style.total}>Total: {subTotal}</span> : <span className={style.total}>Cart Is Empty</span>
+    }
+    
     <div className={style.flexDiv}>
+
     {items?.length ? (
       <>
       <CustomButton
-        onClick={() => {
-          /* navigate("/checkout"); */
-          setToggleState(!toggleState);
-        }}
+        onClick={ handleCart.bind(this,'POST')}
       >
         CHECKOUT
       </CustomButton>
-      <CustomButton onClick={()=>clearCart()} >CLEAR CART</CustomButton>
+      <CustomButton onClick= {handleCart.bind(this,'DELETE')} >CLEAR CART</CustomButton>
       </>
     ) : (
       null
